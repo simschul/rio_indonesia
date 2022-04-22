10e9960ac0347ae7f3df2fe39dd49d16d0ad7d60
Simon Schulte <s.pedro@gmx.de>
2022-04-22 22:03:41 +0200

    code cleaning

--- 2_prepare_filter_shps.R
+++ 2_prepare_filter_shps.R
@@ -22,7 +22,7 @@ library(data.table)
 library(job)
 library(sf)
 library(terra)
-
+library(pbmcapply)
 ############################################################################## # 
 ##### settings #################################################################
 ############################################################################## # 
@@ -30,7 +30,7 @@ options("datatable.print.class" = TRUE)
 theme_set(theme_bw())
 
 species <- c("Acacia_auriculiformis", 'Tectona_grandis', 'Hevea_brasiliensis')
-suitability_classes <- c('S1', 'S2')
+suitability_classes <- c('S1_S2')
 ############################################################################## # 
 ##### load data #############################################################
 ############################################################################## # 
@@ -50,72 +50,72 @@ path2rasters <- './temp_results'
 pars <- vector('list', length(species)) %>% 
   setNames(species)
 
-pars$Acacia_auriculiformis$S1 <- list(
-  tavg_mean = c(23, 30),
-  prec_sum = c(1300,2500),
-  slope_tangent = c(0, 0.08), 
+pars$Acacia_auriculiformis$S1_S2 <- list(
+  tavg_mean = c(21, 34),
+  prec_sum = c(1000,4000),
+  slope_tangent = c(0, 0.15), 
   soil_REF_DEPTH = c(75, Inf), # check this out
-  soil_T_PH_H2O = c(7, 7.5),
+  soil_T_PH_H2O = c(6, 8),
   #soil_S_PH_H2O = c(7, 7.5), 
-  soil_T_CEC_CLAY = c(16, Inf), 
-  soil_T_OC = c(0.4, Inf)
+  soil_T_CEC_CLAY = c(5, Inf), 
+  soil_T_OC = c(0, Inf)
 )
 
-pars$Acacia_auriculiformis$S2 <- list(
-  tavg_mean = c(21, 23, 30, 34),
-  prec_sum = c(1000, 1300, 2500, 4000),
-  slope_tangent = c(0.08, 0.15), 
-  soil_REF_DEPTH = c(75, Inf), # check this out
-  soil_T_PH_H2O = c(6, 7, 7.5, 8),
-  #soil_S_PH_H2O = c(6, 7, 7.5, 8), 
-  soil_T_CEC_CLAY = c(5, 16), 
-  soil_T_OC = c(0, 0.4)
-)
+# pars$Acacia_auriculiformis$S2 <- list(
+#   tavg_mean = c(21, 23, 30, 34),
+#   prec_sum = c(1000, 1300, 2500, 4000),
+#   slope_tangent = c(0.08, 0.15), 
+#   soil_REF_DEPTH = c(75, Inf), # check this out
+#   soil_T_PH_H2O = c(6, 7, 7.5, 8),
+#   #soil_S_PH_H2O = c(6, 7, 7.5, 8), 
+#   soil_T_CEC_CLAY = c(5, 16), 
+#   soil_T_OC = c(0, 0.4)
+# )
 
-pars$Tectona_grandis$S1 <- list(
-  tavg_mean = c(25, 30),
-  prec_sum = c(1500,2000),
-  slope_tangent = c(0, 0.08), 
+pars$Tectona_grandis$S1_S2 <- list(
+  tavg_mean = c(21, 35),
+  prec_sum = c(1250,2250),
+  slope_tangent = c(0, 0.15), 
   soil_REF_DEPTH = c(75, Inf), # check this out
-  soil_T_PH_H2O = c(5.5, 7),
+  soil_T_PH_H2O = c(5, 7.5),
   #soil_S_PH_H2O = c(5.5, 7), 
-  soil_T_CEC_CLAY = c(16, Inf), 
-  soil_T_OC = c(3, Inf)
+  soil_T_CEC_CLAY = c(5, Inf), 
+  soil_T_OC = c(2, Inf)
 )
 
-pars$Tectona_grandis$S2 <- list(
-  tavg_mean = c(21, 25, 30, 35),
-  prec_sum = c(1250, 1500,2000, 2250),
-  slope_tangent = c(0.08, 0.15), 
-  soil_REF_DEPTH = c(75, Inf), # check this out
-  soil_T_PH_H2O = c(5, 5.5, 7, 7.5),
-  #soil_S_PH_H2O = c(5, 5.5, 7, 7.5), 
-  soil_T_CEC_CLAY = c(5, 16), 
-  soil_T_OC = c(2, 3)
-)
+# pars$Tectona_grandis$S2 <- list(
+#   tavg_mean = c(21, 25, 30, 35),
+#   prec_sum = c(1250, 1500,2000, 2250),
+#   slope_tangent = c(0.08, 0.15), 
+#   soil_REF_DEPTH = c(75, Inf), # check this out
+#   soil_T_PH_H2O = c(5, 5.5, 7, 7.5),
+#   #soil_S_PH_H2O = c(5, 5.5, 7, 7.5), 
+#   soil_T_CEC_CLAY = c(5, 16), 
+#   soil_T_OC = c(2, 3)
+# )
 
 
-pars$Hevea_brasiliensis$S1 <- list(
-  tavg_mean = c(26, 30),
-  prec_sum = c(2500,3000),
-  slope_tangent = c(0, 0.08), 
+pars$Hevea_brasiliensis$S1_S2 <- list(
+  tavg_mean = c(24, 34),
+  prec_sum = c(2000,3500),
+  slope_tangent = c(0, 0.16), 
   soil_REF_DEPTH = c(75, Inf), # check this out
-  soil_T_PH_H2O = c(5, 6),
+  soil_T_PH_H2O = c(4.5, 6.5),
   #soil_S_PH_H2O = c(5, 6), 
-  soil_T_CEC_CLAY = c(16, Inf), 
-  soil_T_OC = c(0.8, Inf)
+  soil_T_CEC_CLAY = c(5, Inf), 
+  soil_T_OC = c(0, Inf)
 )
 
-pars$Hevea_brasiliensis$S2 <- list(
-  tavg_mean = c(24, 26, 30, 34),
-  prec_sum = c(2000, 2500, 3000, 3500),
-  slope_tangent = c(0.08, 0.16), 
-  soil_REF_DEPTH = c(75, Inf), # check this out
-  soil_T_PH_H2O = c(4.5, 5, 6, 6.5),
-  #soil_S_PH_H2O = c(4.5, 5, 6, 6.5), 
-  soil_T_CEC_CLAY = c(5, 16), 
-  soil_T_OC = c(0, 0.8)
-)
+# pars$Hevea_brasiliensis$S2 <- list(
+#   tavg_mean = c(24, 26, 30, 34),
+#   prec_sum = c(2000, 2500, 3000, 3500),
+#   slope_tangent = c(0.08, 0.16), 
+#   soil_REF_DEPTH = c(75, Inf), # check this out
+#   soil_T_PH_H2O = c(4.5, 5, 6, 6.5),
+#   #soil_S_PH_H2O = c(4.5, 5, 6, 6.5), 
+#   soil_T_CEC_CLAY = c(5, 16), 
+#   soil_T_OC = c(0, 0.8)
+# )
 
 pars
 
@@ -171,7 +171,7 @@ create_filters <- function(
     raster_files[[i]][!is.na(raster_files[[i]])] <- 1
     
     # resample so that all rasters have same extent
-    if (i > 1) {
+    if (i > 1 & !isTRUE(all.equal(ext(raster_files[[1]]), ext(raster_files[[i]])))) {
       cat('Resample \n')
       raster_files[[i]] <- resample(raster_files[[i]], raster_files[[1]])
     }
@@ -204,176 +204,38 @@ create_filters <- function(
 # apply function for each parameter combination and save results
 par_comb <- expand.grid(specie = species, suitability_class = suitability_classes)
 
-filters <- pbmclapply(1:nrow(par_comb), function(x) {
+
+filters <- pbmclapply(1:nrow(par_comb), function(i) {
   # create directory for results if not already existing
-  dir <- file.path('results', par_comb[x,1], par_comb[x,2])
+  dir <- file.path('results', par_comb[i,1], par_comb[i,2])
   if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
   
   # run create filter function
-  result <- create_filters(pars[[par_comb[x,1]]][[par_comb[x,2]]], 
+  filter_rasters <- create_filters(pars[[par_comb[i,1]]][[par_comb[i,2]]], 
                            raster_files = r_list)
-  
-  # 1. save as raster
-  cat('save as raster\n')
-  terra::writeRaster(result, file.path(dir, 'filters.grd'), 
-                     overwrite = TRUE)
-  
-  
-  
-  # Convert rasters to polygons ==================================================
-  cat('save as shapefile\n')
-  shp_list <- vector('list', length = dim(result)[3]) %>% 
-    setNames(names(result))
-  
-  
-  for (k in 1:length(shp_list)) {
-    #convert SpatRaster -> SpatVector
-    temp_shp <- as.polygons(result[[k]])
-    tempdir <- tempdir(check = TRUE)
-    # save as .shp
-    writeVector(temp_shp, filename = file.path(tempdir, 'temp.shp'), 
-                overwrite = TRUE)
-    # read as sf
-    shp_list[[k]] <- st_read(file.path(tempdir, 'temp.shp'))
-  }
-  shp_list <- lapply(shp_list, st_make_valid) %>% 
-    setNames(names(shp_list))
-  shp_list <- lapply(shp_list, st_buffer, dist = 0) %>% 
-    setNames(names(shp_list))
+
+  # Convert rasters to polygons
+  cat('convert to shapefile and save \n')
+  filter_shps <- lapply(filter_rasters, function(j) {
+    j <- as.polygons(j)
+    j <- st_as_sf(j)
+    j <- st_make_valid(j)
+    j <- st_buffer(j, 0)
+    
+    st_write(j, file.path(dir, paste0('filter_', names(j)[1], '.shp')), 
+             append = FALSE)
+    return(j)
+  })
+  names(filter_shps) <- names(filter_rasters)
   
   # save results
-  saveRDS(shp_list, file.path(dir, 'filters.RData'))
+  saveRDS(filter_shps, file.path(dir, 'filters.RData'))
   
-  return(result)
-}, mc.cores = 6)
+  return(filter_shps)
+}, mc.cores = 3)
 
 
-### old stuff 
-
-for (i in 1:length(pars)) {
-  cat(names(pars)[i], '\n')
-  for (j in 1:length(pars[[i]])) {
-    cat(names(pars[[i]])[j], '\n')
-    result <- create_filters(pars[[i]][[j]], 
-                            raster_files = r_list)
-    
-    plot(result)
-    
-    # create directory for results if not already existing
-    dir <- file.path('results', names(pars)[i], names(pars[[i]])[j])
-    if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
-    
-    # 1. save as raster
-    cat('save as raster\n')
-    terra::writeRaster(result, file.path(dir, 'filters.grd'), 
-                       overwrite = TRUE)
-    
-    
-    
-    # Convert rasters to polygons ==================================================
-    cat('save as shapefile\n')
-    shp_list <- vector('list', length = dim(result)[3]) %>% 
-      setNames(names(result))
-    
-    
-    for (k in 1:length(shp_list)) {
-      #convert SpatRaster -> SpatVector
-      temp_shp <- as.polygons(result[[k]])
-      tempdir <- tempdir(check = TRUE)
-      # save as .shp
-      writeVector(temp_shp, filename = file.path(tempdir, 'temp.shp'), 
-                  overwrite = TRUE)
-      # read as sf
-      shp_list[[k]] <- st_read(file.path(tempdir, 'temp.shp'))
-    }
-    shp_list <- lapply(shp_list, st_make_valid) %>% 
-      setNames(names(shp_list))
-    shp_list <- lapply(shp_list, st_buffer, dist = 0) %>% 
-      setNames(names(shp_list))
-    
-    # save results
-    saveRDS(shp_list, file.path(dir, 'filters.RData'))
-  
-  }
-}
-
-
-
-
-
-mapview(test %>% lapply(raster::raster))
-plot(test)
-# create one raster filter that combines all 3 other filters ===================
-
-# convert to terra raster
-#r_list2 <- lapply(r_list, rast) # not working somehow
-
-# quick and dirty fix: 
-tempdir <- tempdir()
-for (i in 1:length(r_list)) {
-  raster::writeRaster(r_list[[i]], file.path(tempdir, paste0(names(r_list)[i], '.tif')), 
-                      overwrite = TRUE)
-  cat(i, '')
-}
-r_list2 = lapply(1:length(r_list), function(x) {
-    rast(file.path(tempdir, paste0(names(r_list)[x], '.tif')))
-  })
-names(r_list2) = names(r_list)
-
-
-# extend the slope raster
-# r_list2$slope_tangent <- terra::extend(r_list2$slope_tangent,
-#                                        r_list$tavg_mean)
-# 
-# 
-
-# save stack
-terra::writeRaster(r_stack3, './temp_results/filters_HeveaBrasiliensis_S1S2.grd', 
-                   overwrite = TRUE)
-
-
-
-# Convert rasters to polygons ==================================================
-shp_list <- vector('list', length = dim(r_stack3)[3]) %>% 
-  setNames(names(r_stack3))
-
-
-for (i in 1:length(shp_list)) {
-  cat(i, '')
-  #convert SpatRaster -> SpatVector
-  temp_shp <- as.polygons(r_stack3[[i]])
-  tempdir <- tempdir(check = TRUE)
-  # save as .shp
-  writeVector(temp_shp, filename = file.path(tempdir, 'temp.shp'), 
-              overwrite = TRUE)
-  # read as sf
-  shp_list[[i]] <- st_read(file.path(tempdir, 'temp.shp'))
-  
-  # combine all features into one (to speed up the intersect-calculations)
-  #shp_list[[i]] <- st_union(shp_list[[i]])
-}
-
-
-# save results
-saveRDS(shp_list, './temp_results/filters_HeveaBrasiliensis_S1S2.RData')
-
-mapview(shp_list)
-
-# for (i in 1:length(v)) {
-#   st_write(shp_list[[i]], paste0('./temp_results/filter_', 
-#                                  names(shp_list)[i], 
-#                                  '_',
-#                                  paste(filter[[i]], collapse = '-'),
-#                                  '.shp'))
-#   
-# }
-# 
-
-
-
-
-
-
+### end 
 
 
 
--- 3_join_maps_parallel2.R
+++ 3_join_maps_parallel2.R
@@ -79,10 +79,18 @@ filter_degraded_land_map <- function(dl, filter, save_dir = NULL, n_cores = 1) {
       st_intersection(dl, filter[[i]])
     }, error = function(e) print(e))
     
+    results[[i]] <- st_collection_extract(results[[i]], 'POLYGON')
+    results[[i]] <- st_cast(results[[i]], 'POLYGON')
+    
     if (!is.null(save_dir)){
       if (!dir.exists(save_dir)) dir.create(save_dir)
       saveRDS(results[[i]], 
               file.path(save_dir, paste0('DL_filtered_', names(filter)[i], '.RData')))
+      st_write(results[[i]], 
+              file.path(save_dir, paste0('DL_filtered_', names(filter)[i], '.shp')), 
+              append = FALSE)
+      
+      
     }
     toc()
   }
@@ -92,7 +100,7 @@ filter_degraded_land_map <- function(dl, filter, save_dir = NULL, n_cores = 1) {
 
 # 2. run function for all species and S classes ===============================
 species <- c("Acacia_auriculiformis", 'Tectona_grandis', 'Hevea_brasiliensis')
-suitability_classes <- c('S1', 'S2')
+suitability_classes <- c('S1_S2')
 
 par_comb <- expand.grid(specie = species, suitability_class = suitability_classes)
 
@@ -103,7 +111,7 @@ dl_filtered <- pbmclapply(1:nrow(par_comb), function(x) {
   result <- filter_degraded_land_map(dl, filter, 
                                      save_dir = file.path(path, 'DL35'))
   return(result)
-}, mc.cores = 6)
+}, mc.cores = min(6, nrow(par_comb)))
 
 
 # analyse results ===============================
@@ -112,176 +120,11 @@ files <- list.files('./results', 'DL_filtered_combined.RData', recursive = TRUE,
            full.names = TRUE)
 
 data <- lapply(files, readRDS)
+names(data) <- files
 lapply(data, class)
 
-#### jun k ===============================
-# make a grid over shp and select all grid cells that intersect the shp
-grid <- st_make_grid(dl, n = 9)
-grid <- grid[dl]
+lapply(data, function(x) sum(st_area(x)) %>% units::set_units(ha))
 
-# stop using s2, otherwise will cause topology error (see: )
-sf_use_s2(TRUE)
-
-# run in parallel
-n_cores <- 7
-
-filtered_maps <- vector('list', length = length(filters)) %>% 
-  setNames(names(filters))
-
-tic('parallel')
-# plan(multisession(workers = 6))
-# 
-# split_results <- future_lapply(
-#   1:length(grid), function(x) {
-#     within <- st_intersects(dl, grid[x]) %>% 
-#       sapply(any)
-#     
-#     for (i in 1:length(filters)) {
-#       filtered_maps[[i]] <- tryCatch({
-#         st_intersection(dl[within == 1,], 
-#                         filters[[i]])
-#       }, error = function(e) print(e))
-#       
-#     }
-#     return(filtered_maps)
-#   }  
-# )
-
-split_results <- pbmcapply::pbmclapply(
-  1:length(grid), function(x) {
-    within <- st_intersects(dl, grid[x]) %>% 
-      sapply(any)
-    
-    for (i in 1:length(filters)) {
-      filtered_maps[[i]] <- tryCatch({
-        st_intersection(dl[within == 1,], 
-                        filters[[i]])
-      }, error = function(e) print(e))
-      
-    }
-    return(filtered_maps)
-  }, mc.cores = n_cores)
-toc()
-# 579.891  sec
-
-# check for error messages
-sapply(split_results, function(x) {
-  sapply(x, function(y) {
-    'sf' %in% class(y)    
-  })
-}) %>% mean
-# should be all TRUE
-
-## begin test
-# grids_broken <- c(25, 35, 36, 42, 44)
-# x <- grids_broken[2]
-# within <- st_intersects(dl, grid[x]) %>% 
-#   sapply(any)
-# 
-# test2 <- dl[within == 1,] %>% st_is_valid()
-# mapview(dl[within ==1,])
-# 
-# sf_use_s2(TRUE)
-# 
-# test3 <- vector('list', nrow(dl[within ==1,]))
-# for (i in 1:nrow(dl[within ==1,])){
-#   cat(i, '')
-#   test3[[i]] <- tryCatch({
-#     st_intersection(dl[within == 1, ][i,], filters$combined)
-#   }, error = function(e) print(e))
-# }
-# allok <- sapply(test3, function(x) 'sf' %in% class(x))
-# mean(allok)
-# mapview(dl[within ==1, ][!allok,])
-
-## end test
-
-
-
-# combine results
-results <- pbmcapply::pbmclapply(names(filters), function(x) {
-  temp <- sapply(split_results, '[', x)
-  temp <- temp[sapply(temp, function(x) {'sf' %in% class(x)})]
-  temp <- do.call('rbind', temp)
-  st_make_valid(temp)
-}, mc.cores = 7) %>% 
-  setNames(names(filters))
-
-# [1] "Error : arguments have different crs\n"
-# attr(,"class")
-# [1] "try-error"
-# attr(,"condition")
-# <simpleError: arguments have different crs>
-
-
-#mapview(results)
-
-
-# union geometries
-results2 <- pbmcapply::pbmclapply(results, function(x) {
-  x[, c('DN', 'geometry')] %>% 
-    as.data.table %>% 
-    .[, list(geometry = st_combine(geometry)), by = DN] %>% 
-    st_as_sf
-}, mc.cores = 7)
-
-
-
-lapply(results, function(x) sum(st_area(x)) %>% units::set_units(ha))
-
-# my output: 
-# $tavg_mean
-# 6200079 [ha]
-# 
-# $prec_sum
-# 4483993 [ha]
-# 
-# $slope_tangent
-# 6175453 [ha]
-# 
-# $soil_REF_DEPTH
-# 5987475 [ha]
-# 
-# $soil_T_PH_H2O
-# 5769478 [ha]
-# 
-# $soil_S_PH_H2O
-# 5691945 [ha]
-# 
-# $combined
-# 3871329 [ha]
-
-
-# save results
-saveRDS(results2, './temp_results/cstocks35_fixed_200122_cleaned100m2_filtered_v5.RData')
-
-mapview(results2$combined)
-# as shp
-for (i in 1:length(results)) {
-  st_write(results[[i]], paste0('./temp_results/',
-                                'DL35_', 
-                                'HeveaBrasiliensis_',
-                                'S1S2_',
-                                names(results)[i],
-                                '_v5.shp'), 
-           append = FALSE
-  )
-}
-
-# calc area by DN
-
-results3 <- lapply(results, function(x) {
-  x <- as.data.table(x)
-  x[, area := st_area(geometry)]
-  x <- x[, list(area = sum(area)), by = DN]
-  setorderv(x, 'DN')
-  return(x)
-})
-
-
-rio::export(
-  results3, file = './temp_results/area_by_DN_and_variable.xlsx'
-)
 
 
 # end  
